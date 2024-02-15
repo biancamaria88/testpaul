488412a44f0a5f4546ffbf2dc66dc6f369c5abad
biancamaria88 <153809368+biancamaria88@users.noreply.github.com>
2024-02-15 07:16:14 +0000

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import './App.css';
+import React, {useState} from "react";
+import './App.jsx';
 
 function App() {
+    const[contor, setContor]=useState(25);
+
+    
+const increment=() => {
+  setContor(contor + 5);
+};
+const decrement= () => {
+setContor(contor - 1);
+
+} 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    <h1>Valoarea contorului: {contor}</h1>
+    <button onClick={increment}>adunare</button>
+    <span>{contor}</span>
+    <button onClick={decrement}>scadere</button>
+  </div>
+  ) ;  
 }
+
 
 export default App;
