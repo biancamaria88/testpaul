df08924e9d92677b5a44c58ef0f23b31dca28dad
biancamaria88 <153809368+biancamaria88@users.noreply.github.com>
2024-03-28 08:14:50 +0000

    mesaj

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import './App.jsx';
+import './App.css';
 
 function App() {
     const[contor, setContor]=useState(25);
@@ -12,13 +13,16 @@ const decrement= () => {
 setContor(contor - 1);
 
 } 
-  
+const divideByTwo=()=> {
+  setContor(contor/2);
+}  
   return (
     <div className="App">
     <h1>Valoarea contorului: {contor}</h1>
-    <button onClick={increment}>adunare</button>
+    <button onClick={increment} className="adunare">adunare</button>
     <span>{contor}</span>
-    <button onClick={decrement}>scadere</button>
+    <button onClick={decrement} className= "heart">scadere</button>
+    <button onClicl={divideByTwo} className="adunare">impartire</button>
   </div>
   ) ;  
 }
